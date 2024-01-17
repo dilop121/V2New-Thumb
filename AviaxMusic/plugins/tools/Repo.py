80dacecac243ae1c0e004c3e3a8497390d511268
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-17 09:53:53 +0530

    Update Repo.py

--- AviaxMusic/plugins/tools/Repo.py
+++ AviaxMusic/plugins/tools/Repo.py
@@ -4,21 +4,25 @@ from pyrogram import Client, filters
 from pyrogram.types import InputMediaVideo, InlineKeyboardMarkup, InlineKeyboardButton
 
 
+repo_button = InlineKeyboardButton("• ʀᴇᴘᴏ •", callback_data="my_source")
 close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="close")
 
 @app.on_message(filters.command("repo"))
 def start_command(client, message):
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("ʀᴇᴘᴏ❥", callback_data='repo_calls')]
+        [repo_button, close_button]
     ])
+    message.reply_text("ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ᴍᴀᴅᴀʀᴊᴀᴀᴛ ᴛᴏ sᴇᴇ ᴛʜᴇ ʀᴇᴘᴏ!", reply_markup=keyboard)
 
-    message.reply_markup = keyboard
+@app.on_message(filters.text & ~filters.command)
+def echo_message(client, message):
+    message.reply_text(message.text)
 
-@app.on_callback_query(filters.regex("repo_calls"))
-async def gib_repo_callback(_, callback_query):
-    media_url = "https://graph.org/file/52b2315b843584a3c4532.mp4"
-
+@app.on_callback_query(filters.regex("my_source"))
+async def my_repo_callback(_, callback_query):
     await callback_query.edit_message_media(
-        media=InputMediaVideo(media_url),
-        reply_markup=InlineKeyboardMarkup([[close_button]])
+        media=InputMediaVideo("https://graph.org/file/52b2315b843584a3c4532.mp4", has_spoiler=True),
+        reply_markup=InlineKeyboardMarkup([[close_button]]),
     )
+
+
