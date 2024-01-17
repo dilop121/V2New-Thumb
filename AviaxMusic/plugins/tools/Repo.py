87f6c3e85405f317a67fee666230d62eaecca6af
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-17 10:54:58 +0530

    Update Repo.py

--- AviaxMusic/plugins/tools/Repo.py
+++ AviaxMusic/plugins/tools/Repo.py
@@ -1,105 +1,18 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InputMediaVideo
-
+from pyrogram.types import InputMediaVideo
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AviaxMusic import app
-from config import OWNER_ID
-import config
-import random 
-from pyrogram.types import Message
-
-#⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-#⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-
-
-start_txt = """**
-๏🤖 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ɢɪᴛʜᴜʙ & ʜᴇʀᴏᴋᴜ ᴄᴏɴᴛʀᴏʟ ʙᴏᴛ! 🚀
-
-๏ᴛʜɪs ʙᴏᴛ sɪᴍᴘʟɪғɪᴇs ʏᴏᴜʀ   
-ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ Jᴏᴜʀɴᴇʏ ʙʏ ɪɴᴛᴇɢʀᴀᴛɪɴɢ ɢɪᴛʜᴜʙ ʀᴇᴄᴇɪᴠᴇ ɪɴsᴛᴀɴᴛ ɢɪᴛʜᴜʙ ᴜᴘᴅᴀᴛᴇs ᴀɴᴅ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ʜᴇʀᴏᴋᴜ
-
-
-๏ᴅᴇᴘʟᴏʏᴍᴇɴᴛs ᴇғғᴏʀᴛʟᴇssʟʏ
-ᴛʏᴘᴇ /help ᴛᴏ ᴇxᴘʟᴏʀᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ sᴜᴘᴇʀᴄʜᴀʀɢᴇ ʏᴏᴜʀ ᴡᴏʀᴋғʟᴏᴡ. ʟᴇᴛ's ᴍᴀᴋᴇ ᴄᴏᴅɪɴɢ ᴀɴᴅ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ ᴀ ʙʀᴇᴇᴢᴇ! 💻🔧 #ɢɪᴛʜᴜʙ #ʜᴇʀᴏᴋᴜ #ᴅᴇᴠᴛᴏᴏʟs"
-**"""
-
-
-
 
 @app.on_message(filters.command("repo"))
-async def start(_, msg):
-    buttons = [
-        [
-          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/HEROKUFREECC"),
-          InlineKeyboardButton("ᴅᴇᴠ", url="https://t.me/iam_daxx")
-        ],
-        [
-          InlineKeyboardButton("ʜᴇʟᴘ", callback_data="githelp"),
-          InlineKeyboardButton("ʀᴇᴘᴏ", callback_data="gib_sources")
-        ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await msg.reply_photo(
-        photo="https://graph.org/file/ecbeac5889f9542f32469.jpg",
-        caption=start_txt,
-        reply_markup=reply_markup
-    )
+def start_command(client, message):
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("ʀᴇᴘᴏ❥", callback_data='gib_source')]
+    ])
 
+    message.reply_text("ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ᴍᴀᴅᴀʀᴊᴀᴀᴛ ᴛᴏ sᴇᴇ ᴛʜᴇ ʀᴇᴘᴏ!", reply_markup=keyboard)
 
-
-#➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪#➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪#➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪
-
-#➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪
-
-@app.on_callback_query()
-def callback_query_handler(client, query):
-    if query.data == 'githelp':
-        ghelp_text = (
-            "GitHub Control Bot Commands:\n"
-            "/start - Start the bot\n"
-            "/help - Display this help message\n"
-            "/allrepo - List your GitHub repositories\n"
-            "/create_repo - Create a new GitHub repository\n"
-            "/delrepo - Delete a GitHub repository\n"
-            "/add_collaborator - Add a collaborator to a GitHub repository\n"
-            "/remove_collaborator - Remove a collaborator from a GitHub repository"
-        )
-
-        
-        buttons = [
-            [
-                InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close_data")
-            ]
-        ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-
-        
-        query.message.edit_text(ghelp_text, reply_markup=reply_markup)
-
-#----------------------------------------------------------------------------------------------
-#--------------------------------------------------
-
-
-# Additional callback for closing the message
-@app.on_callback_query(filters.regex("^close_data"))
-async def close_callback(_, query):
-    chat_id = query.message.chat.id
-    await query.message.delete()
-# incoming msg
-
-@app.on_message(filters.private & filters.incoming)
-async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id ==OWNER_ID:
-        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
-        
-
-
-
-
-
-
-@app.on_callback_query(filters.regex("gib_sources"))
-async def gibs_repo_callback(_, callback_query):
+@app.on_callback_query(filters.regex("gib_source"))
+async def gib_repo_callback(_, callback_query):
     await callback_query.edit_message_media(
         media=InputMediaVideo("https://graph.org/file/52b2315b843584a3c4532.mp4"),
         reply_markup=InlineKeyboardMarkup(
@@ -109,4 +22,4 @@ async def gibs_repo_callback(_, callback_query):
         ),
     )
 
-close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="back")
+close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="close")
