1adc3ebba90a1055d4ab7a58b3cb23d03243d140
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-17 10:16:41 +0530

    Update Repo.py

--- AviaxMusic/plugins/tools/Repo.py
+++ AviaxMusic/plugins/tools/Repo.py
@@ -5,8 +5,8 @@ from pyrogram.types import InputMediaVideo, InlineKeyboardMarkup, InlineKeyboard
 repo_button = InlineKeyboardButton("• ʀᴇᴘᴏ •", callback_data="my_source")
 close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="close")
 
-@app.on_message(filters.command("repo"))
-def start_command(client, message):
+@app.on_message(filters.command(["repo", "repolist", "repository"]))
+def repo_command(client, message):
     keyboard = InlineKeyboardMarkup([
         [repo_button, close_button]
     ])
@@ -15,7 +15,7 @@ def start_command(client, message):
 @app.on_callback_query(filters.regex("my_source"))
 async def my_repo_callback(_, callback_query: CallbackQuery):
     try:
-        await callback_query.answer()  # Respond to the button press
+        await callback_query.answer()
         await callback_query.edit_message_media(
             media=InputMediaVideo("https://graph.org/file/52b2315b843584a3c4532.mp4"),
             reply_markup=InlineKeyboardMarkup([[close_button]]),
