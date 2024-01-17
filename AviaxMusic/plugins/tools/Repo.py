018696c3e29858aebf4701ac2cb530d6e1d8f17f
Dil <126651350+dilop121@users.noreply.github.com>
2024-01-17 09:36:20 +0530

    Update Repo.py

--- AviaxMusic/plugins/tools/Repo.py
+++ AviaxMusic/plugins/tools/Repo.py
@@ -5,21 +5,20 @@ from AviaxMusic import app
 @app.on_message(filters.command("repo"))
 def start_command(client, message):
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("ʀᴇᴘᴏ❥", callback_data='repo_callback')]
+        [InlineKeyboardButton("ʀᴇᴘᴏ❥", callback_data='repo_calls')]
     ])
 
     message.reply_text("ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ᴍᴀᴅᴀʀᴊᴀᴀᴛ ᴛᴏ sᴇᴇ ᴛʜᴇ ʀᴇᴘᴏ!", reply_markup=keyboard)
 
-@app.on_callback_query(filters.regex(r'^repo_callback'))
-def handle_repo_callback(client, callback_query):
-    user_id = callback_query.from_user.id
-    video_file = "https://graph.org/file/52b2315b843584a3c4532.mp4"
+@app.on_callback_query(filters.regex("repo_calls"))
+async def gib_repo_callback(_, callback_query):
+    await callback_query.edit_message_media(
+        media=InputMediaVideo("https://graph.org/file/52b2315b843584a3c4532.mp4"),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [close_button]
+            ]
+        ),
+    )
 
-    try:
-        client.send_video(
-            chat_id=user_id,
-            video=video_file,
-            reply_to_message_id=callback_query.message.message_id
-        )
-    except Exception as e:
-        print(f"Error sending video: {e}")
+close_button = InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="close")
